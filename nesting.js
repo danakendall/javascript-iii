347bfb75ab4f4ee9ca7cd97f1e93f2f019de81c2
danakendall <30518457+danakendall@users.noreply.github.com>
2017-08-24 19:36:25 -0600

    employeeUpdater

--- nesting.js
+++ nesting.js
@@ -1,5 +1,5 @@
 // =========================
-// NESTING 
+// NESTING
 // =========================
 
 // === PROBLEM 1 ===========
@@ -31,13 +31,24 @@ var employees = [{
   "department": "Research and Development"
 }]
 
-// Create a function called 'employeeUpdater'. employeeUpdater will loop over the array above and perform the following:
+// Create a function called 'employeeUpdater'. employeeUpdater will loop over the array above
+//and perform the following:
 //  1. If employee's first name is Theo, remove that employee because he just got fired.
 //  2. If the employee's first name is Lorie, change her department to 'HR'.
 //  3. Return the updated employee array.
 
 // Code here
-
+function employeeUpdater() {
+  for (var i=0; i<employees.length; i++) {
+    if (employees[i].firstName==="Theo" ) {
+      employees.splice(i, 1);
+    }
+    else if (employees[i].firstName==="Lorie") {
+      employees[i].department = 'HR';
+    }
+  }
+  return employees;
+}
 
 
 
@@ -47,7 +58,7 @@ var employees = [{
 // === PROBLEM 2 ==========
 
 
-// The array below represents IDs tied to reported workplace accidents. An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are. 
+// The array below represents IDs tied to reported workplace accidents. An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are.
 // 1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
 // 2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array.
 // 3. Return the updated array.
@@ -78,7 +89,7 @@ var cat = {
     }]
 }
 
-// Fluffy has two friends, Grumpy and Lazy Bones. 
+// Fluffy has two friends, Grumpy and Lazy Bones.
 // 1. Assign the value of Grumpy's 2nd activity to the variable below.
 // 2. Assign fluffy2ndFriend the name of Fluffy's 2nd friend.
 
@@ -135,14 +146,10 @@ var myCar = {
 // === PROBLEM 5 ==========
 
 // Below is an array of arrays. Use two for loops.
-// 1. Write a function called 'looper'. 
-// 2. 'looper' should loop over the arrays. 
+// 1. Write a function called 'looper'.
+// 2. 'looper' should loop over the arrays.
 // 3. If the number is odd, replace it with 'odd'.
 //    If the number is even, replace it with 'even'.
 // 4. Return the modified numsArr.
 
 var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
-
-
-
-
